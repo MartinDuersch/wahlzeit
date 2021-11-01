f3a5c3799d00866bb5097ccbc3b9ce1b1ea4d628
Flamekeks <martin.duersch@t-online.de>
2021-11-02 00:16:26 +0100

    Added getDistance, isEqual, getters, setters

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -15,4 +15,50 @@ public class Coordinate {
         this.z = z;
     }
 
+    public double getDistance(Coordinate coordinate) {
+        return Math.sqrt(
+            Math.pow(this.getX() - coordinate.getX(), 2) +
+            Math.pow(this.getY() - coordinate.getY(), 2) +
+            Math.pow(this.getZ() - coordinate.getZ(), 2));
+    }
+
+    public boolean isEqual(Coordinate coordinate) {
+        if (coordinate instanceof Coordinate == false) {
+            return false;
+        } else {
+            return (this.getX() == coordinate.getX() &&
+                    this.getY() == coordinate.getY() &&
+                    this.getZ() == coordinate.getZ());
+        }
+    }
+
+    @Override
+    public boolean equals(Object coordinate) {
+        return isEqual((Coordinate) coordinate);
+    }
+
+    public double getX() {
+		return x;
+	}
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
 }
